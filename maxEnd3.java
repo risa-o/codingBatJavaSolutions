d48f48f105600248ce18a386a9fabfda8d738191
risa-o <risa.orsinger@gmail.com>
2019-09-24 12:29:26 -0500

    Coding Bat Solutions Array-1

---
+++ maxEnd3.java
@@ -0,0 +1,13 @@
+public int[] maxEnd3(int[] nums) {
+  if (nums[0] > nums[2]){
+    nums[1] = nums[0];
+    nums[2] = nums[0];
+    return nums;
+  }
+  else {
+    nums[0] = nums[2];
+    nums[1] = nums[2];
+    return nums;
+  }
+
+}
